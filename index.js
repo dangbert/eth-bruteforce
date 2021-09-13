67d42170692f144ae2064cc75dec8f359bb918c2
Daniel Engbert <end1@umbc.edu>
2021-09-13 19:56:45 +0000

    Base version working. Run 'yarn test' to validate

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
+/*
+ * run program with:
+ *   node index.js /path/to/keystorefile /path/to/pwList.txt
+ * example:
+ *   node index.js example---UTC--2018-04-29T10-08-25.072Z--1f7c98090febf46155496a370002a10af7eb6766 exampleList.txt
+ */
+
+
 // based on the code linked from here https://ethereum.stackexchange.com/a/47036
 
 const Wallet = require('ethereumjs-wallet'), fs = require('fs');
 
 /**
- * please work
+ * exit program and print password if it works to decrypt a given ETH wallet.
  */
 const tryPassword = (walletData, password)  => {
   try {
     const myWallet = Wallet.fromV3(walletData, password, true);
-    console.log("Address: " + myWallet.getAddress().toString('hex')) ;
+    console.log("Wallet address: " + myWallet.getAddress().toString('hex'));
     console.log(`password worked!!! "${password}"`);
+    //console.log("Private Key: " + myWallet.getPrivateKey().toString('hex')) 
     process.exit(0);
   } catch(e) {
     const err = e.toString();
     if (e.toString() !== 'Error: Key derivation failed - possibly wrong passphrase') {
       throw e;
     }
-    //console.log('err: ' + err);
   }
-  //console.log("Private Key: " + myWallet.getPrivateKey().toString('hex')) 
 
 };
 
 
-const exampleFile = "./example---UTC--2018-04-29T10-08-25.072Z--1f7c98090febf46155496a370002a10af7eb6766"
-const exampleData = fs.readFileSync(exampleFile).toString();
-
-tryPassword(exampleData, 'wrongpass');
-tryPassword(exampleData, 'password123');
-
-//const myWallet = Wallet.fromV3(fs.readFileSync(utcFile).toString(), password, true);
-//console.log("Private Key: " + myWallet.getPrivateKey().toString('hex')) 
-//console.log("Address: " + myWallet.getAddress().toString('hex')) 
+const fname = process.argv.slice(2, 3).toString();
+console.log(`reading keystore file "${fname}"`);
 
 
+const walletData = fs.readFileSync(fname).toString();
+// test for example file:
+//tryPassword(walletData, 'wrongpass');
+//tryPassword(walletData, 'password123');
 
+// path to text file of possible passwords ('\n' delimeted)
+const listName = process.argv.slice(3, 4).toString();
+console.log(`reading list of possible passwords: "${listName}"`);
+let list = fs.readFileSync(listName).toString().split('\n');
+list = list.filter(p => p); // remove any empty lines ("")
+
+for (let p of list) {
+  //console.log(`trying pass "${p}"`);
+  tryPassword(walletData, p);
+}
 
